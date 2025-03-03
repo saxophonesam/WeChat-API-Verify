1cd468d76e4a11e8aa0faa656779b2812b9282d4
saxophonesam <sam.yucheng@gmail.com>
2025-03-03 19:32:11 +0800

    adding all my files

--- src/app.py
+++ src/app.py
@@ -1,16 +1,3 @@
-# import os
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def verify():
-#     return "WW_verify_3lX7NSa3vJ4Rt4QX"
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
-
 from flask import Flask, send_from_directory
 import os
 
