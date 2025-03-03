2c6e90033507c9870edea63ba04c09bb40151488
saxophonesam <sam.yucheng@gmail.com>
2025-03-03 19:24:16 +0800

    adding all my files

--- src/app.py
+++ src/app.py
@@ -1,23 +1,28 @@
-# from flask import Flask, send_from_directory
 # import os
+# from flask import Flask
 
 # app = Flask(__name__)
-# # server = app.server
-# @app.route('/WW_verify_3lX7NSa3vJ4Rt4QX.txt')
+
+# @app.route("/")
 # def verify():
-#     return send_from_directory(os.getcwd(), 'WW_verify_3lX7NSa3vJ4Rt4QX.txt')
+#     return "WW_verify_3lX7NSa3vJ4Rt4QX"
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
 
+from flask import Flask, send_from_directory
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def verify():
-    return "WW_verify_3lX7NSa3vJ4Rt4QX"
+def hello():
+    return "Hello, WeChat API Verify!"
+
+@app.route("/WW_verify_3lX7NSa3vJ4Rt4QX.txt")
+def wechat_verify():
+    return send_from_directory("static", "WW_verify_3lX7NSa3vJ4Rt4QX.txt")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
